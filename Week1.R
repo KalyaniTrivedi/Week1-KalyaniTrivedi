57d7cebccbd5fbc9073659a9671afbde022ca0ca
Kalyani Trivedi <ktrivedi@seattleu.edu>
2022-06-30 09:53:26 -0700

    Commit with the following changes - assigning the output to variables - updating box plot code - uploading the box plot

--- Week1.R
+++ Week1.R
@@ -1,4 +1,4 @@
-library("haven")
+library(haven)
 # Loading the data
 nfhs <- read_dta("data/IAHR52FL.dta")
 # Total number of households
@@ -9,19 +9,19 @@ variables <- ncol(nfhs)
 # Total number of variables in the sub section of the data.
 sub_variable_count <- ncol(subset(nfhs, select = hhid:shstruc))
 # Display the histogram
-ggplot(data = nfhs,
-       mapping = aes(x = hv009), binwidth = 1) +
-  geom_histogram() +
-  xlab("Number of household members")
+histogram_plot <- ggplot(data = nfhs, 
+                         mapping = aes(x = hv009), binwidth = 1) +
+                    geom_histogram() +
+                    xlab("Number of household members")
 # Urban household data
 urban_household <- filter(nfhs, hv025 == 1)
 # Box plot code
-boxplot(hv026~hv025, 
-        data=urban_household, notch=TRUE, 
-        main="Mean urban household type", 
-        xlab="Household type", ylab="Urban household", 
-        col="Orange", horizontal = TRUE)
+box_plot <- ggplot(data = urban_household, 
+                   aes(x=hv009, y=as.factor(hv026))) + 
+              geom_boxplot(notch = TRUE) + 
+              xlab("Number of household memebers") + 
+              ylab("Type of household")
 # Mean and median for household members
-nfhs%>%
-  group_by(hv026)%>%
-  summarise(mean = mean(hv009), median = median(hv009))
+summary_data <- nfhs%>%
+                  group_by(hv026)%>%
+                    summarise(mean = mean(hv009), median = median(hv009))
